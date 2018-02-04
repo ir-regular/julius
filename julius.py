03f5d8c72703500c8b229d1e6b70c2ef9f3377e6
jane-olszewska <jane.olszewska@duedil.com>
2018-02-04 10:36:08 +0000

    Keep passing the list of phrases and pauses as a list, to avoid assumptions about its structure

--- julius.py
+++ julius.py
@@ -1,6 +1,6 @@
 import argparse
 from julius.source.phrasefile import read_phrases
-from julius.voice.say import join_with_pauses, dictate
+from julius.voice.say import insert_pauses, dictate
 
 def main():
     parser = argparse.ArgumentParser()
@@ -10,11 +10,10 @@ def main():
     args = parser.parse_args()
 
     phrases = read_phrases(args.phrases)
-    output = join_with_pauses(phrases, args.pause)
+    phrases = insert_pauses(phrases, args.pause)
 
     try:
-        # todo: could feed it phrases one by one, to allow the user to pause
-        dictate(output)
+        dictate(phrases)
     except KeyboardInterrupt:
         exit(0)
 
--- julius/voice/say.py
+++ julius/voice/say.py
@@ -1,10 +1,18 @@
 import subprocess
+import itertools
 
 def dictate(parts):
-    subprocess.run(['say', '-v', 'Daniel'], stdout=subprocess.PIPE, input=parts, encoding='utf-8')
+    # todo: could feed it phrases one by one, to allow the user to pause
+    input = " ".join(parts)
+    subprocess.run(['say', '-v', 'Daniel'], stdout=subprocess.PIPE, input=input, encoding='utf-8')
 
-def join_with_pauses(parts, pauseLength):
-    return pause(pauseLength).join(parts)
+def insert_pauses(parts, pauseLength):
+    if pauseLength == 0:
+        return parts
+
+    pausePart = pause(pauseLength)
+    parts = [(part, pausePart) for part in parts]
+    return list(itertools.chain.from_iterable(parts))
 
 def pause(pauseLength):
-    return " [[slnc {0}]] ".format(pauseLength) if (pauseLength > 0) else " "
+    return "[[slnc {0}]]".format(pauseLength)
